fb7e88392907aa05e0afa97deab5704af0aa3b2b
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-08-04 22:27:35 -0300

    Adicionando novos serviços - #34

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,4 +1,7 @@
 import React from 'react'
+
+import { parse, v4 as uuidv4 } from 'uuid';
+
 import { useParams } from 'react-router-dom';
 import { useState, useEffect} from 'react';
 
@@ -6,6 +9,7 @@ import Container from '../layout/Container';
 import Loading from '../layout/Loading';
 import ProjectForm from '../project/ProjectForm';
 import Message from '../layout/Message';
+import ServiceForm from '../service/ServiceForm';
 import styles from './Project.module.css';
 
 
@@ -66,7 +70,48 @@ function Project() {
       })
       .catch(err => console.log(err))
 
+    }
 
+    function createService(project) {
+        setMessage('')
+
+      // last service
+      const lastService = project.services[project.services.length - 1]
+
+      lastService.id = uuidv4()
+
+      const lastServiceCost = lastService.cost 
+
+      const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+
+      // maximum value validation
+
+      if(newCost >  parseFloat(project.budget)) {
+        setMessage('Orçamento ultrapassado, verifique o valor do serviço !!')
+        setType('error')
+        project.services.pop()
+        return false  
+      }
+
+      // add service cost to project total cost 
+
+      project.cost = newCost
+
+      //  update project
+
+      fetch(`http://localhost:5000/projects/${project.id}`, {
+
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(project),
+      }).then( (resp) => resp.json())
+      .then((data) => {
+        // exibir os serviços
+          console.log(data)
+      })
+      .catch( err =>  console.log(err))
     }
       
     function toggleProjectForm() {
@@ -76,11 +121,13 @@ function Project() {
     function toggleServiceForm() {
       setShowServiceForm(!showServiceForm)
     }
+
+
     
   return (
     <>
       {project.name ?  ( 
-        <div className={styles.project_details}>
+        <div className={styles.project_details}> 
           <Container customClass="column">
             {message && <Message type={type} msg={message} /> }
             <div className={styles.details_container}> 
@@ -119,8 +166,11 @@ function Project() {
                 <button className={styles.btn} onClick={toggleServiceForm}>
                 {!showServiceForm? 'Adicionar serviço' : 'Fechar'}
                 </button>
+
                 <div className={styles.project_info}>
-                  {showServiceForm && ( <div>Formulário do serviço</div> )}
+                  {showServiceForm && 
+                    <ServiceForm handleSubmit={createService} btnText="Adicionar Serviço" projectData={project}  />
+                   }
                  </div> 
             </div>    
 
@@ -130,7 +180,12 @@ function Project() {
                 <p>Itens de Serviços...</p>
             </Container>
 
+            
+
           </Container>
+
+         
+
         </div>
         
       ): ( <Loading /> )}
